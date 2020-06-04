15dd5335b79deb4f86a1b8a863edbf970dbd7443
pepe454 <17fryerd@gmail.com>
2020-06-04 16:13:04 -0400

    major refactor

--- recipe_scraper.py
+++ recipe_scraper.py
@@ -16,53 +16,46 @@ def print_recipe(title, author, ingredients, instructions):
   print("-" * len(header))
   print()
 
-def food_network(dish):
-  ''' 
-  Food Network - https://www.foodnetwork.com
-  Search items must begin, be separated by, and end with '-'
+def find_recipes(url, prefix,
+                result_type, result_class, 
+                author_type, author_class, 
+                title_type, title_class, 
+                ingredient_type, ingredient_class, 
+                instruction_type, instruction_class):
   '''
-  url = f"https://www.foodnetwork.com/search/{'-'.join(dish.split()) + '-'}"
+  Generic way to get recipes:
+    1. Search for recipe, limiting results only to recipe
+    2. For each result, extract the link
+    3. Follow each link and extract title, author, recipe, and instructions
+  '''
 
-  # get all links for the search results
+  # search for a recipe and extract the link of each result
   results = get_url(url)
-  results = results.find_all('section', class_='o-RecipeResult')
-  links = [result.find('a')['href'] for result in results]
+  results = results.find_all(result_type, result_class)
+  links = [prefix + result.find('a')['href'] for result in results]
 
-  # get the recipe for each result: includes author, title, ingredients, and methods
-  for link in links:
-    results2 = get_url('https:' + link)
-
-    # get data from the page
-    author = results2.find_all('span', class_='o-Attribution__a-Name')[0].text.strip()
-    title = results2.find_all('span', class_='o-AssetTitle__a-HeadlineText')[0].text.strip()
-    ingredients = [ingredient.text.strip() for ingredient in results2.find_all('p', class_='o-Ingredients__a-Ingredient')]
-    instructions = [instruction.text.strip() for instruction in results2.find_all('li', class_='o-Method__m-Step')]
-
-    print_recipe(title, author, ingredients, instructions)
-
-def bon_appetite(dish):
-  '''
-  Bon Appetite - https://www.bonappetit.com/ 
-  Search items separated by %20
-  '''
-  url = "https://www.bonappetit.com" # need to use this later for relative links
-  results = get_url(f"{url}/search/{'%20'.join(dish.split())}?content=recipe")
-  results = results.find_all('article', class_='recipe-content-card')
-  links = [url + result.find('a')['href'] for result in results]
-  
+  # find title, author, and recipe for each link result
   for link in links:
     results2 = get_url(link)
-
-    # extract information
-    author = results2.find_all('span', class_='contributor-name')
+    # extract data from the secondary links
+    author = results2.find_all(author_type, class_=author_class)
     author = author[0].text.strip() if len(author) > 0 else "Unknown"
-    title = results2.find_all('a', class_='top-anchor')[0].text.strip()
-    ingredients = [ingredient.text.strip() for ingredient in results2.find_all('li', class_='ingredient')]
-    instructions = [instruction.text.strip() for instruction in results2.find_all('li', class_='step')]
+    title = results2.find_all(title_type, class_=title_class)
+    title = title[0].text.strip() if len(title) > 0 else "Unknown"
+    ingredients = [ingredient.text.strip() for ingredient in results2.find_all(ingredient_type, class_=ingredient_class)]
+    instructions = [instruction.text.strip() for instruction in results2.find_all(instruction_type, class_=instruction_class)]
 
     print_recipe(title, author, ingredients, instructions)
 
 if __name__ == '__main__':
   dish = input("What would you like to eat tonight? ")
-  food_network(dish)
-  bon_appetite(dish)
+  # Bon Appetite
+  find_recipes(f"https://www.bonappetit.com/search/{'%20'.join(dish.split())}?content=recipe", 'https://www.bonappetit.com', 
+              'article', 'recipe-content-card', 'span', 'contributor-name', 'a', 'top-anchor', 
+              'li', 'ingredient', 'li','step')
+
+  # Food Network
+  find_recipes(f"https://www.foodnetwork.com/search/{'-'.join(dish.split()) + '-'}", 'https:',
+               'section', 'o-RecipeResult', 'span', 'o-Attribution__a-Name', 
+               'span', 'o-AssetTitle__a-HeadlineText', 'p', 'o-Ingredients__a-Ingredient',
+               'li', 'o-Method__m-Step')
